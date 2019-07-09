f6ee4aa9b111bebdda2c1a61c1c09ca1629ec152
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2019-07-08 23:25:10 -0400

    React: Create a Controlled Form

--- react.js
+++ react.js
@@ -1,20 +1,39 @@
-const INCREMENT = 'INCREMENT' // define a constant for increment action types
-const DECREMENT = 'DECREMENT' // define a constant for decrement action types
-
-const counterReducer = (state = 0, action) => {
-    switch (action.type){
-     case INCREMENT:
-       return  state = state + 1 
-     case DECREMENT:
-       return state = state - 1 
-     default:
-        return state
-    }
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    // change code below this line
+  {event.preventDefault()};
+  this.setState({
+    submit:this.state.input
+  })
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          { /* change code below this line */ }
+            <input type="text" value={this.state.input} onChange ={this.handleChange} />
+          { /* change code above this line */ }
+          <button type='submit'>Submit!</button>
+        </form>
+        { /* change code below this line */ }
+        <h1>{this.state.submit}</h1>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
 };
-// define the counter reducer which will increment or decrement the state based on the action it receives
-
-const incAction = () => { return {type: INCREMENT} }; // define an action creator for incrementing
-
-const decAction = () => ( {type: DECREMENT} ); // define an action creator for decrementing
-
-const store = Redux.createStore (counterReducer); // define the Redux store here, passing in your reducers
